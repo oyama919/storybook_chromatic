6a5bacc176a43d6ecb5e2c1838278745961d430f
oyama919 <oyama919@gmail.com>
2022-05-27 09:23:29 +0900

    example button2

---
+++ src/stories/Button2.stories.js
@@ -0,0 +1,47 @@
+import MyButton from "./Button2.vue";
+
+export default {
+  title: "Example/Button2",
+  component: MyButton,
+  argTypes: {
+    backgroundColor: { control: "color" },
+    size: {
+      control: { type: "select", options: ["small", "medium", "large"] },
+    },
+    onClick: {},
+  },
+};
+
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { MyButton },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: '<my-button v-bind="args" />',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: "Button2",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: "Button2",
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: "large",
+  label: "Button2",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: "small",
+  label: "Button",
+};
